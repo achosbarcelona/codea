886b5163f25ad436017d1bdecfec18e3e30296d4
Chanel Mepschen <chanelmepschen856@hotmail.com>
2018-01-23 10:48:41 +0100

    Made the videos fitting from algarme

--- project-12.php
+++ project-12.php
@@ -23,6 +23,44 @@
     <link rel="stylesheet" href="css/infinite.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/hover.css">
+    <style>
+    .video-flex {
+        display: flex;
+        width: 100%;
+    }
+    .video-wrap_auto {
+        display: flex;
+        justify-content: center;
+        height: 36.3vw;
+    }
+    .video-flex1__forty {
+        height: 100%;
+        width: 36vw;
+        border-top: 2px solid #000;
+        border-bottom: 4px solid #000;
+        border-left: 2px solid #000;
+        border-right: 2px solid #000;
+    }
+    .video-flex1__sixty {
+        width: 64vw;
+        height: 36.3vw;
+        border-top: 2px solid #000;
+        border-bottom: 4px solid #000;
+        border-left: 2px solid #000;
+        border-right: 2px solid #000;
+    }
+    .video-flex2 {
+        display: flex;
+    }
+    .video-flex2 > * {
+        height: 30vw;
+        width: auto;
+    }
+    .video-flex2__fifty {
+        width: 50vw;
+        height: 28.1vw;
+    }
+    </style>
 
     <script src="js/jquery.js"></script>
     <script src="js/projects.js"></script>
@@ -68,11 +106,16 @@
             <img class="sixty" src="img/projects/12_almarge/3.png">
             <!-- <iframe class="sixty" src="https://player.vimeo.com/video/242774786?autoplay=0&title=0&byline=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             <iframe class="forty" src="https://player.vimeo.com/video/248116209?autoplay=0&title=0&byline=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> -->
-            <video class="sixty" src="img/projects/12_almarge/4.mp4" autoplay loop nocontrols></video>
-            <video class="forty" src="img/projects/12_almarge/5.mp4" autoplay loop nocontrols></video>
-
-            <img class="half" src="img/projects/12_almarge/6.png">
-            <video class="videohalf" src="img/projects/12_almarge/7.mp4" autoplay loop nocontrols></video>
+            <div class="video-flex">
+                <video src="img/projects/12_almarge/4.mp4" autoplay loop nocontrols class="video-flex1__sixty"></video>
+                <div class="video-wrap_auto">
+                    <video src="img/projects/12_almarge/5.mp4" autoplay loop nocontrols class="video-flex1__forty"></video>
+                </div>
+            </div>
+            <div class="video-flex2">
+                <img class="video-flex2__fifty" src="img/projects/12_almarge/6.png">
+                <video class="video-flex2__fifty" src="img/projects/12_almarge/7.mp4" autoplay loop nocontrols></video>
+            </div>
             <!-- <iframe class="videohalf" src="https://player.vimeo.com/video/248116745?autoplay=0&title=0&byline=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> -->
         </div>
 
